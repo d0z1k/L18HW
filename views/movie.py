4d63a8b5d9266ad2ccb9456c0bc0fdfe244f4b34
dozik <dozik91@gmail.com>
2022-11-03 21:25:42 +0200

    mvview

---
+++ views/movie.py
@@ -0,0 +1,46 @@
+from flask_restx import Resource, Namespace
+
+movie_ns = Namespace('movie')
+
+
+@movie_ns.route('/')
+@movie_ns.param('director_id')
+@movie_ns.param('genre_id')
+@movie_ns.param('year')
+class MoviesView(Resource):
+    def get(self):
+        """
+        List all movies.
+        """
+
+        return "", 200
+
+    def put(self):
+        """
+        Create a new movie.
+        """
+        return "", 201
+
+
+@movie_ns.route('/<int:movie_id>')
+class MoviesView(Resource):
+    def get(self, movie_id:int):
+        """
+        Get a movie by id.
+        """
+
+        return "", 200
+
+    def put(self, movie_id:int):
+        """
+        Create a new movie.
+        """
+        return "", 201
+
+    def __delete__(self, movie_id:int):
+        """
+        Delete a movie.
+        :param movie_id:
+        :return:
+        """
+        return "", 204
