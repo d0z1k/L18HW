5119a5e7ad432b4cdc6e4d7443fbe921f1a3ffff
Xander <106524913+d0z1k@users.noreply.github.com>
2022-11-08 18:20:47 +0200

    models

--- dao/model/models.py
+++ dao/model/models.py
@@ -4,6 +4,18 @@
 
 from setup_db import db
 
+
+
+class Genre(db.Model):
+    __tablename__ = ‘genre’
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+class Director(db.Model):
+    __tablename__ = ‘director’
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
 class Movie(db.Model):
     __tablename__ = ‘movie’
     id = db.Column(db.Integer, primary_key=True)
@@ -12,7 +24,8 @@ class Movie(db.Model):
     trailer = db.Column(db.String)
     year = db.Column(db.Integer)
     rating = db.Column(db.Float)
-    genre_id = db.Column(db.Integer, db.ForeignKey(""))
-    director_id = db.Column(db.Integer, db.ForeignKey(""))
-
+    genre_id = db.Column(db.Integer, db.ForeignKey(f"{Genre.__tablename__}.id"))
+    director_id = db.Column(db.Integer, db.ForeignKey(f"{Director.__tablename__}.id"))
+    genre = db.relationship("Genre")
+    director = db.relationship("Director")
 
